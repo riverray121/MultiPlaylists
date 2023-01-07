a7a3b4c0bcbb4dc9e09de2c5ed2c605b87fdfc2b
Elijah Retzlaff <eliriver@gmail.com>
2023-01-07 11:09:17 -0800

    Initial Commit

---
+++ MultiPlaylistsV0.3/MultiPlaylistsV0_3App.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlaylistsV0_3App.swift
+//  MultiPlaylistsV0.3
+//
+//  Created by Elijah Retzlaff on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlaylistsV0_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
